bbe19577108aa30bdeecf035d26e810cf0b80a0a
Pepijn <99075810+PepijnDevue@users.noreply.github.com>
2023-04-30 11:00:39 +0200

    Cleaned main.py up a bit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import pyglet
-from pyglet.window import mouse
 
 WINDOW_SIZE = 720
 SQUARE_SIZE = WINDOW_SIZE//90
@@ -18,11 +17,18 @@ def start_pve():
     print("PVE")
 
 def open_menu(window):
+    """
+    Open the menu page where the player can choose the gamemode, pvp or pve
+
+    Args:
+        window (pyglet.window.Window): The window in which the menu should be displayed
+    """
     title = pyglet.text.Label('Dammen',font_size=54, x=WINDOW_SIZE//2, y=WINDOW_SIZE//1.3, anchor_x='center', anchor_y='center', color=BLACK)
     menu_background = pyglet.shapes.Rectangle(x=0, y=0, width=WINDOW_SIZE, height=WINDOW_SIZE, color=YELLOW)
 
     pve_button = pyglet.shapes.Rectangle(x=WINDOW_SIZE//2-BUTTON_WIDTH//2, y=WINDOW_SIZE//2-BUTTON_HEIGHT//2, width=BUTTON_WIDTH, height=BUTTON_HEIGHT,color=GREY)
     pve_label = pyglet.text.Label('1 Speler', font_size=28, x=WINDOW_SIZE//2, y=WINDOW_SIZE//2+5, anchor_x='center', anchor_y='center', color=BLACK)
+
     pvp_button = pyglet.shapes.Rectangle(x=WINDOW_SIZE//2-BUTTON_WIDTH//2, y=WINDOW_SIZE//3-BUTTON_HEIGHT//2, width=BUTTON_WIDTH, height=BUTTON_HEIGHT,color=GREY)
     pvp_label = pyglet.text.Label('2 Spelers', font_size=28, x=WINDOW_SIZE//2, y=WINDOW_SIZE//3+5, anchor_x='center', anchor_y='center', color=BLACK)
 
@@ -39,9 +45,9 @@ def open_menu(window):
 
     @window.event
     def on_mouse_press(x,y, button, modifiers):
-        if(x > pvp_button.x and x < pvp_button.x+pvp_button.width and y > pvp_button.y and y < pvp_button.y+pvp_button.height):
+        if(x > pvp_button.x and x < pvp_button.x+pvp_button.width and y > pvp_button.y and y < pvp_button.y+pvp_button.height and button == pyglet.window.mouse.LEFT):
             print("PVP")
-        elif(x > pve_button.x and x < pve_button.x+pve_button.width and y > pve_button.y and y < pve_button.y+pve_button.height):
+        elif(x > pve_button.x and x < pve_button.x+pve_button.width and y > pve_button.y and y < pve_button.y+pve_button.height and button == pyglet.window.mouse.LEFT):
             print("PVE")
         
 
@@ -65,6 +71,6 @@ def open_menu(window):
 #             square.draw()
 
 if __name__ == '__main__':
-    window = pyglet.window.Window(WINDOW_SIZE, WINDOW_SIZE, "Dammen menu")
+    window = pyglet.window.Window(WINDOW_SIZE, WINDOW_SIZE, "Dammen")
     open_menu(window)
     pyglet.app.run()
