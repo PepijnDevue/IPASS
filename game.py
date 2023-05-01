bbc7288180a7ae0359e76821f4d9a525932a5fe0
Pepijn <99075810+PepijnDevue@users.noreply.github.com>
2023-05-01 10:58:13 +0200

    Starting with classes

--- game.py
+++ game.py
@@ -1,5 +1,16 @@
+class Piece:
+    type = 'P'
+    def __init__(self, player):
+        self.player = player
+
+    def valid_moves(pos):
+        pass
+
+# class Board:
+#     def __init__(self, board=)
+
 def start_pvp():
     print("PVP")
 
 def start_pve():
-    print("PVE")  
+    print("PVE")
